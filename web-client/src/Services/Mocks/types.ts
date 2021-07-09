f0228a2d9b50acb444bf668eaf679864ad70e0e9
Alessio Pragliola <alessio.pragliola@sighup.io>
2021-07-09 17:22:33 +0200

    license fix

--- web-client/src/Services/Mocks/types.ts
+++ web-client/src/Services/Mocks/types.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2021 SIGHUP s.r.l All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 import {AnyFactories, AnyModels, BelongsTo, FactoryDefinition, ModelDefinition, Registry} from "miragejs/-types";
 import Schema from "miragejs/orm/schema";
 
