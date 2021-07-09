3b0a9dc3ad1c100815d0d74994bc264abb6ab3f3
Alessio Pragliola <alessio.pragliola@sighup.io>
2021-07-09 17:22:33 +0200

    license fix

--- web-client/src/Components/ApplicationStatus/Component.tsx
+++ web-client/src/Components/ApplicationStatus/Component.tsx
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2021 SIGHUP s.r.l All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 import React, {useEffect} from "react";
 import { ClusterStatus } from "../ClusterStatus";
 import { BrowserRouter as Router, Route, Switch, RouteComponentProps } from "react-router-dom";
