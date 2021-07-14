9bd1392260124c65eff23c3cfde1ad9c2904e902
AndreaDellaValle <andreareva@gmail.com>
2021-07-14 16:18:35 +0200

    chore: rectangle function setup

--- web-client/src/Components/UptimeBar/Component.tsx
+++ web-client/src/Components/UptimeBar/Component.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 
 export const UptimeBar = () => {
-  const viewboxWidth = 100
+  const viewboxWidth = 100;
+  const successColor = '#00a39a';
+  const dangerColor = '#BD271E';
 
-  
+  const RectangleUnit = () => {
+    return (
+      <rect height="34" width="1" x="97" y="0" fill={successColor} className="uptime-day component-mvm98gtxvb9b day-0" data-html="true"></rect>
+    )
+  }
 
   return (
     <>
       <svg viewBox={`0 0 ${viewboxWidth} 3`} xmlns="http://www.w3.org/2000/svg">
-        <rect height="34" width="1" x="99" y="0" fill="#28a745" className="uptime-day component-mvm98gtxvb9b day-0" data-html="true"></rect>
+        <RectangleUnit />
+        <rect height="100" width="1" x="99" y="0" fill={dangerColor} className="uptime-day component-mvm98gtxvb9b day-0" data-html="true"></rect>
       </svg>
     </>
   )
