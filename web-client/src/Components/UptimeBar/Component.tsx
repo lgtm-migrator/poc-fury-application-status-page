a19c1d5687b20fe001169b08242714f4717a7df8
Alessio Pragliola <alessio.pragliola@sighup.io>
2021-07-14 17:01:18 +0200

    uptime pt 0.5

--- web-client/src/Components/UptimeBar/Component.tsx
+++ web-client/src/Components/UptimeBar/Component.tsx
@@ -1,21 +1,47 @@
 import React from "react";
+import {HealthCheckStatus} from "../types";
 
-export const UptimeBar = () => {
-  const viewboxWidth = 100;
+interface RectangleUnitProps {
+  status: HealthCheckStatus;
+  x: number;
+  width: number;
+}
+
+interface UptimeBarProps {
+  itemList: any[];
+  viewBoxWidth: number;
+  itemOffset: number;
+  itemWidth: number;
+}
+
+const isSuccess = (status: HealthCheckStatus) => {
   const successColor = '#00a39a';
   const dangerColor = '#BD271E';
 
-  const RectangleUnit = () => {
-    return (
-      <rect height="34" width="1" x="97" y="0" fill={successColor} className="uptime-day component-mvm98gtxvb9b day-0" data-html="true"></rect>
-    )
+  if (status === "Complete") {
+    return successColor;
   }
 
+  return dangerColor;
+}
+
+const RectangleUnit = (props: RectangleUnitProps) => {
+  return (
+    <rect height="34" width={props.width} x={props.x} y="0" fill={isSuccess(props.status)} className="uptime-day day-0" data-html="true"/>
+  )
+}
+
+export const UptimeBar = (props: UptimeBarProps) => {
   return (
     <>
-      <svg viewBox={`0 0 ${viewboxWidth} 3`} xmlns="http://www.w3.org/2000/svg">
-        <RectangleUnit />
-        <rect height="100" width="1" x="99" y="0" fill={dangerColor} className="uptime-day component-mvm98gtxvb9b day-0" data-html="true"></rect>
+      <svg viewBox={`0 0 ${props.viewBoxWidth} 3`} xmlns="http://www.w3.org/2000/svg">
+        {
+          props.itemList.map((item, index) => {
+            return (
+              <RectangleUnit status={"Complete"} width={props.itemWidth} x={props.viewBoxWidth - index*props.itemOffset - props.itemWidth} />
+            )
+          })
+        }
       </svg>
     </>
   )
--- web-client/src/Components/UptimeBar/Container.tsx
+++ web-client/src/Components/UptimeBar/Container.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { UptimeBar } from "./Component";
 
 export const UptimeBarContainer = () => {
+  const itemList = new Array(100).fill({
+    status: "Complete"
+  });
+
   return (
-    <UptimeBar />
+    <UptimeBar viewBoxWidth={100} itemList={itemList} itemOffset={1} itemWidth={0.5}/>
   )
 }
