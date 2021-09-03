d6077065a133c4ef3567033d7f597a8de548de0f
Alessio Pragliola <alessio.pragliola@sighup.io>
2021-09-03 16:38:01 +0200

    fix: cypress multiple server

--- development/e2e-up-ci.sh
+++ development/e2e-up-ci.sh
@@ -40,7 +40,7 @@ echo "Scenario 1"
 
 make port-forward &
 
-docker run -i -e CYPRESS_BASE_URL -e CYPRESS_VIDEO --entrypoint=bash -d --network host --name="${CYPRESS_ID}" cypress/included:8.3.0
+docker run -i -e CYPRESS_BASE_URL -e CYPRESS_VIDEO -e DISPLAY: --entrypoint=bash -d --network host --name="${CYPRESS_ID}" cypress/included:8.3.0
 
 trap cleanup-cypress EXIT
 
